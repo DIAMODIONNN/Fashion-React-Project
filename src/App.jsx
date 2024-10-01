e467ededd3631e3537a90cadaf1e4c8eae99e943
Amr <amrmohamed23033@gmail.com>
2024-10-01 21:01:53 +0300

    first

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Button } from '@material-tailwind/react'
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <div className='text-center'>
+        
+        <h1>App</h1>
+        <Button color='amber'>Click here</Button>
+      </div>
+  )
+}
+
+export default App
